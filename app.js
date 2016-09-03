6150d991688646f980eb4bec693fc2a460ba2dfa
Sujil Maharjan <Sujil Maharjan>
2016-09-02 21:16:39 -0500

    Tried beautify

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname, 'public')));
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,18 @@
-$(document).ready(function () {
-    
+$(document).ready(function() {
+
     // This controls the hover for travel timeline
     $('#travelTimeline img').hover(function() {
-        $(this).removeClass("desaturate").animate({zoom: '120%'}, "fast");
-    },
-        function () {
-        $(this).addClass("desaturate").animate({zoom: '100%'}, "fast");
-    });
+            $(this).removeClass("desaturate").animate({
+                zoom: '120%'
+            }, "fast");
+        },
+        function() {
+            $(this).addClass("desaturate").animate({
+                zoom: '100%'
+            }, "fast");
+        });
 
-    $('#experience tr').on('click', function () {
+    $('#experience tr').on('click', function() {
         var jobDetail = $(this).find('.display-option');
         jobDetail.toggle('fast');
 
@@ -16,7 +20,7 @@ $(document).ready(function () {
         logoDisplay.toggle();
     });
 
-    $('#extracurricular td').on('click', function () {
+    $('#extracurricular td').on('click', function() {
         var jobDetail = $(this).find('.display-option');
         jobDetail.toggle('fast');
 
@@ -30,13 +34,17 @@ $(document).ready(function () {
 
         // If the skill is high, then turn the background to green and put the width to 100%
         if (isHigh) {
-            $(this).addClass('skill-high').animate({width: '100%'}, 'slow')
-        }
-        else if (isMedium){
-            $(this).addClass('skill-medium').animate({width: '50%'}, "slow");
-        }
-        else {
-            $(this).addClass('skill-low').animate({width: '30%'},"slow");
+            $(this).addClass('skill-high').animate({
+                width: '100%'
+            }, 'slow')
+        } else if (isMedium) {
+            $(this).addClass('skill-medium').animate({
+                width: '50%'
+            }, "slow");
+        } else {
+            $(this).addClass('skill-low').animate({
+                width: '30%'
+            }, "slow");
         }
     });
 
@@ -44,18 +52,20 @@ $(document).ready(function () {
         alert("ok");
         var mainFile = this;
         $.ajax({
-            type:'POST',
-            url :"/processImage",
+            type: 'POST',
+            url: "/processImage",
             data: {
                 inputFile: mainFile
             },
             dataType: 'json',
             success: function(data) {
-                    console.log('success',data);
+                console.log('success', data);
 
             },
-            error:function(exception){alert('Exeption:'+exception);}
-        }); 
+            error: function(exception) {
+                alert('Exeption:' + exception);
+            }
+        });
         e.preventDefault();
     });
 
